b756babfa62ac41c1a8b28ac0ea099c1a57bcc6b
GOH0012 <39984985+GOH0012@users.noreply.github.com>
2018-09-07 10:54:04 +0700

    uptate

--- TestOpShift/Controllers/HomeController.cs
+++ TestOpShift/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace TestOpShift.Controllers
         }
 
         public IActionResult Contact()
-        {
+        { 
             ViewData["Message"] = "Your contact page.";
 
             return View();
