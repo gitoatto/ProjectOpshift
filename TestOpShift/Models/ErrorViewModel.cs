803bf6baacacc37fc9f7158975a150735b496b7d
GOH0012 <39984985+GOH0012@users.noreply.github.com>
2018-09-07 10:35:22 +0700

    Add project files.

---
+++ TestOpShift/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestOpShift.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
